bf040b30c3018eae94e4887d5e54eaeabcc693e8
wudian <84020702@qq.com>
2018-07-03 18:35:21 +0800

    x

--- shell/commit.sh
+++ shell/commit.sh
@@ -4,16 +4,9 @@ git pull
 sleep 1
 
 
-rm shell/chain/chain 
-
-rm shell/api/api 
-
-rm shell/midapi/midapi 
-
-rm shell/midware/midware 
 
 git add --all .
 git commit -m"x"
-git push origin master-evm:master-evm
+git push origin master:master
 
 # git config credential.helper store
