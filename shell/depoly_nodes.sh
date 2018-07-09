e29c7ccbb4912f93fe36b83a5ea54b265bf495ae
root <wudian@zhongan.io>
2018-07-09 17:20:08 +0800

    x

--- shell/depoly_nodes.sh
+++ shell/depoly_nodes.sh
@@ -4,7 +4,7 @@ address=865c395bfd104394b786a264662d02177897391aba1155f854cb1065b6a444e5
 passwd=h0pe
 path=~/.ann_runtime/
 midapi_node=10.253.160.117 #管控
-nodes=(10.253.112.132 10.253.164.121 10.253.115.1 10.253.168.160 ${midapi_node})
+nodes=(10.253.112.132 10.253.164.121 10.253.115.1 10.253.168.160 10.253.160.117)
 bin=~/advertise-bin
 
 cd depoly_nodes
